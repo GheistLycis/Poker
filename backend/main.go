56d8539c5d063e94b2e735f4f8682f839474f608
Bruno Capitelli <bruno.nitro48@gmail.com>
2026-08-01 12:26:21 -0300

    fix: server message handling

--- backend/main.go
+++ backend/main.go
@@ -20,69 +20,41 @@ const (
 	CLIENT Origin = "CLIENT"
 )
 
-// Define your request and response structs
-type Message struct {
+type Message[T any] struct {
 	Origin Origin `json:"origin"`
 	Type  string `json:"type"`
-	Payload map[int]*string `json:"payload"`
+	Payload T `json:"payload"`
 }
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all origins for testing
+		return true
 	},
 }
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// Upgrade HTTP connection to WebSocket
 	conn, err := upgrader.Upgrade(w, r, nil)
+	
 	if err != nil {
-		log.Println("Upgrade error:", err)
+		log.Println("upgrade error:", err)
 		return
 	}
+	log.Println("client connected:", conn.RemoteAddr())
+	defer log.Println("handler exited:", conn.RemoteAddr())
 	defer conn.Close()
-
-	// Receive and send loop
 	for {
-		var msg Message
-
-		// ReadJSON automatically decodes the incoming JSON payload into the struct
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
+		var msg Message[any]
+		
+		if err := conn.ReadJSON(&msg); err != nil {
+			log.Printf("read error (%T): %v", err, err) 
 			break
 		}
-
-		log.Printf("Received message: %s\n", msg.Payload)
-		payload := map[int]*string{}
-
-		user := "user"
-		opponent := "opponent"
-
-		payload[0] = &user
-		payload[1] = &opponent
-
-
-		// Prepare a response struct
-		reply := Message{
-			Origin:  "SERVER",
-			Type: "match.seats",
-			Payload: payload,
-		}
-
-		// WriteJSON automatically encodes the struct to JSON and sends it
-		err = conn.WriteJSON(reply)
-		if err != nil {
-			log.Println("Write error:", err)
-			break
-		}
+		log.Printf("RECEIVED: type=%s payload=%+v", msg.Type, msg.Payload)
 	}
 }
 
 func main() {
-	http.HandleFunc("/api", handleWebSocket)
+	http.HandleFunc("/", handleWebSocket)
 	log.Println("Server started on :3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'ws://localhost:3000/api',
+  apiUrl: 'ws://localhost:3000',
 };
