06faf6424f72957b9b89ee8a68455c1e63093308
Bruno Capitelli <bruno.nitro48@gmail.com>
2026-08-22 10:58:35 -0300

    fix: minor

--- frontend/src/app/services/match/match.ts
+++ frontend/src/app/services/match/match.ts
@@ -1,5 +1,5 @@
 import { computed, inject, Service } from '@angular/core';
-import type { PlayerAction} from '@app-types/PlayerAction';
+import type { PlayerAction } from '@app-types/PlayerAction';
 import { PlayerActionEnum } from '@app-types/PlayerAction';
 import { ApiService } from '@services/api/api';
 import { WebSocketConnStateEnum } from '@services/api/types/ConnState';
@@ -12,7 +12,9 @@ export class MatchService {
   private apiService = inject(ApiService);
 
   isLoading = computed(() => this.apiService.connState() === WebSocketConnStateEnum.CONNECTING);
-  seats$ = this.apiService.getMessages('match.seats').pipe(shareReplay());
+  seats$ = this.apiService
+    .getMessages('match.seats')
+    .pipe(shareReplay({ bufferSize: 1, refCount: false }));
   opponents$ = combineLatest([
     this.apiService.getMessages('opponents.info'),
     this.apiService
@@ -28,13 +30,15 @@ export class MatchService {
 
       return opponents;
     }),
-    shareReplay(),
+    shareReplay({ bufferSize: 1, refCount: false }),
   );
   seatTurn$ = this.apiService.getMessages('match.seat-turn').pipe(
     map((msg) => msg.seatIndex),
-    shareReplay(),
+    shareReplay({ bufferSize: 1, refCount: false }),
   );
-  revealedCards$ = this.apiService.getMessages('match.table-cards').pipe(shareReplay());
+  revealedCards$ = this.apiService
+    .getMessages('match.table-cards')
+    .pipe(shareReplay({ bufferSize: 1, refCount: false }));
 
   isPlayerTurn(playerSeat: number) {
     return this.seatTurn$.pipe(map((seatTurn) => seatTurn === playerSeat));
