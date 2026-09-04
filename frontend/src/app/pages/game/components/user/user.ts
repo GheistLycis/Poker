b548d0d74ce3ae60e766cd36de4b6c26fdd661e4
Bruno Roberto Brandão Capitelli <bruno.nitro48@gmail.com>
2026-09-04 12:31:47 -0300

    bet slider

--- frontend/src/app/pages/game/components/user/user.ts
+++ frontend/src/app/pages/game/components/user/user.ts
@@ -1,18 +1,27 @@
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { outputFromObservable, toObservable } from '@angular/core/rxjs-interop';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, model } from '@angular/core';
+import { outputFromObservable, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import type { PlayerAction } from '@app-types/PlayerAction';
 import { PlayerActionEnum } from '@app-types/PlayerAction';
 import { MatchService } from '@services/match/match';
 import { UserService } from '@services/user/user';
 import { HlmButtonImports } from '@ui/button';
+import { HlmLabel } from '@ui/label';
+import { HlmSliderImports } from '@ui/slider';
 import { filter, map, switchMap } from 'rxjs';
 import { PlayerActionPipe } from '../../../../pipes/player-action/player-action-pipe';
 import { CardsHand } from '../cards-hand/cards-hand';
 
 @Component({
   selector: 'app-user',
-  imports: [PlayerActionPipe, CardsHand, AsyncPipe, CurrencyPipe, HlmButtonImports],
+  imports: [
+    PlayerActionPipe,
+    CardsHand,
+    CurrencyPipe,
+    HlmButtonImports,
+    HlmSliderImports,
+    HlmLabel,
+  ],
   templateUrl: './user.html',
 })
 export class User {
@@ -22,17 +31,35 @@ export class User {
   private userService = inject(UserService);
   private matchService = inject(MatchService);
 
-  user$ = toObservable(this.userService.user).pipe(filter((user) => !!user));
-  isUserTurn$ = this.user$.pipe(
-    switchMap((user) => this.matchService.isPlayerTurn(user.seatIndex)),
+  user = this.userService.user;
+  private user$ = toObservable(this.userService.user).pipe(filter((user) => !!user));
+  isUserTurn = toSignal(
+    this.user$.pipe(switchMap((user) => this.matchService.isPlayerTurn(user.seatIndex))),
   );
   userWon = outputFromObservable(
     this.user$.pipe(
       switchMap((user) => this.matchService.playerWon(user.id).pipe(map(() => user))),
     ),
   );
+  bet = model<[number]>([0]);
 
-  sendAction(action: PlayerAction, amount?: number) {
-    this.matchService.registerUserAction(action, amount);
+  incrementBet(amount: number, operation: '-' | '+') {
+    let final = this.bet()[0];
+
+    if (operation === '-') {
+      final -= amount;
+      if (final < 0) final = 0;
+    } else {
+      const curr = this.user()!.score;
+
+      final += amount;
+      if (final > curr) final = curr;
+    }
+
+    this.bet.set([final]);
+  }
+
+  sendAction(action: PlayerAction) {
+    this.matchService.registerUserAction(action, this.bet()[0] || undefined);
   }
 }
--- frontend/src/app/services/match/match.ts
+++ frontend/src/app/services/match/match.ts
@@ -17,22 +17,9 @@ export class MatchService {
   seats$ = this.apiService
     .getMessages('match.seats')
     .pipe(shareReplay({ bufferSize: 1, refCount: false }));
-  opponents$ = this.apiService.getMessages('opponents.info').pipe(
-    distinctUntilChanged((prev, curr) => {
-      if (prev.length !== curr.length) return false;
-
-      const sortedPrev = prev.toSorted((a, b) => a.id.localeCompare(b.id));
-      const sortedCurr = curr.toSorted((a, b) => a.id.localeCompare(b.id));
-
-      for (let i = 0; i < sortedPrev.length; i++) {
-        if (this.hasPlayerChangedPipe.transform([sortedPrev[i], sortedCurr[i]])) {
-          return false;
-        }
-      }
-      return true;
-    }),
-    shareReplay({ bufferSize: 1, refCount: false }),
-  );
+  opponents$ = this.apiService
+    .getMessages('opponents.info')
+    .pipe(shareReplay({ bufferSize: 1, refCount: false }));
   seatTurn$ = this.apiService.getMessages('match.seat-turn').pipe(
     map((msg) => msg.seatIndex),
     shareReplay({ bufferSize: 1, refCount: false }),
@@ -55,6 +42,13 @@ export class MatchService {
   getPlayerAtSeat(seat: SeatIndex) {
     return combineLatest([this.opponents$, this.seats$]).pipe(
       map(([opponents, seats]) => opponents.find(({ id }) => id === seats[seat])),
+      distinctUntilChanged((prev, curr) => {
+        if (!prev && !!curr) return false;
+        if (!!prev && !curr) return false;
+        if (!prev && !curr) return true;
+
+        return !this.hasPlayerChangedPipe.transform([prev!, curr!]);
+      }),
     );
   }
 
