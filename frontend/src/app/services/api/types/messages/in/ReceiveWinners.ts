9f8a256aeade7f5fb6255448b7b08178a5ed9bbe
Bruno Roberto Brandão Capitelli <bruno.nitro48@gmail.com>
2026-09-04 18:26:35 -0300

    checkpoint commit

--- frontend/src/app/services/api/types/messages/in/ReceiveWinners.ts
+++ frontend/src/app/services/api/types/messages/in/ReceiveWinners.ts
@@ -1,7 +1,13 @@
+import type { Card } from '@app-types/Card';
+import type { Hand } from '@app-types/Hand';
 import type { Player } from '@app-types/Player';
 import type { WebSocketIncomingMessage } from './WebSocketIncomingMessage';
 
 export interface ReceiveWinners extends WebSocketIncomingMessage {
   type: 'match.winners';
-  payload: Player['id'][];
+  payload: {
+    id: Player['id'];
+    winningHand: Hand;
+    winningCards: Card[];
+  }[];
 }
